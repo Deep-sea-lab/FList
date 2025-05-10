3636c7260d4c61067da9f32ae43b4c7186bad413
Deep-sea-lab <3800907852@qq.com>
2025-05-10 08:18:37 +0000

    😅

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -41,7 +41,7 @@ export default defineUserConfig({
         // 仓库所有者的仓库名
         repository: "FList",
         // github 授权 Token, process.env.xxx  xxx 是环境变量名称。可以通过设置 githubToken 环境变量来配置
-        authorizationToken: "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",
+        authorizationToken: "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",
         // 分页大小，不懂得话就当作取最新的多少个标签吧。
         per_page: 10,
       }),
